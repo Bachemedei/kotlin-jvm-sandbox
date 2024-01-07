86e90cc41edf321f88f74808e84ef95dd82b184e
Bronwyn Achemedei <bronwyn.1992@gmail.com>
2024-01-07 13:24:08 +0800

    Update Kotlin, use junit for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 group = "dev.bcmedeiros"
@@ -20,5 +24,6 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.2")
 
     testImplementation(kotlin("test"))
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = "5.5.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
