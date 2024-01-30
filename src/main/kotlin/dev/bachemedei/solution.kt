6fcc09d5db927d2b23bb71ec89f5222c8e5b888a
Bronwyn Achemedei <bronwyn.1992@gmail.com>
2024-01-30 08:25:08 +0800

    Solved

--- src/main/kotlin/dev/bachemedei/solution.kt
+++ src/main/kotlin/dev/bachemedei/solution.kt
@@ -1,5 +1,19 @@
 package dev.bachemedei
 
-fun solution() {
-    // TODO
+fun invertTree(root: TreeNode?): TreeNode? {
+    return root.invert()
+}
+
+fun TreeNode?.invert(): TreeNode? {
+    if (this == null) return null
+    val node = TreeNode(`val`)
+    node.left = right.also { it.invert() }
+    node.right = left.also { it.invert() }
+    return node
+}
+
+data class TreeNode(var `val`: Int) {
+    val value = `val`
+    var left: TreeNode? = null
+    var right: TreeNode? = null
 }
